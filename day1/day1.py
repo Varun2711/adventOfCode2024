4de2843322ad63ea62975749903eee8322d6df85
Varun Patel <rvjpatel99@gmail.com>
2024-12-03 15:23:05 -0800

    completed day 1 challenge

---
+++ day1/day1.py
@@ -0,0 +1,45 @@
+# Advent of code 2024
+# Day 1
+# Find total distance
+
+
+# Part one
+left = []
+right = []
+
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        l, r = line.split()
+
+        left.append(int(l))
+        right.append(int(r))
+
+
+left.sort()
+right.sort()
+
+total_distance = sum([abs(x-y) for x, y in zip(left, right)])
+
+print(total_distance)
+
+
+# part 2
+
+counts = {}
+
+for n in left:
+    counts[n] = 0
+
+for n in right:
+    if n in counts:
+        counts[n] += 1
+    
+
+s = 0
+for key, val in counts.items():
+    s += (key * val)
+
+print(s)
+
+
+
